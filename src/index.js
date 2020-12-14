1d79a2ee5d2c5a69db875c1aaa2bb0bf65ae6dd4
Chrislearns <drainc4@yahoo.com>
2020-12-14 02:36:04 -0800

    First Commit. Finished fetch api setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Hello extends React.Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   error: null,
+   isLoaded:false,
+   items: null
+  };
+ }
+
+ componentDidMount() {
+  fetch("https://cors-anywhere.herokuapp.com/https://www.rescuetime.com/anapi/daily_summary_feed?key=B63i1WHTfyrn9M68vC73HBkV099Lnse9NPvLOAkY")
+  .then(response => response.json())
+  .then(
+       (data) => {
+        this.setState({
+         isLoaded:true,
+         items:data.items
+        })
+        console.log(data);
+       },
+       (error) => {
+        this.setState({
+         isLoaded:true,
+         error
+        });
+       }
+       
+  )
+ }
+
+  
+ 
+ render() {
+  const {error, isLoaded, items} = this.state;
+  if (error) {
+   return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+   return <div>Loading...</div>
+
+  }else { return (
+   <p>{items}</p>
+  );
+  }
+ }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
